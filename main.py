ec2701fe3d0292b0caf6479af14dd4d0d0edce1d
Ivan <ceo@ivan-victorovich.ru>
2025-04-03 23:35:27 +0300

    -second_commit

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
-import requests
+import aiohttp
+import asyncio
 
-def get_text(url):
-    response = requests.get(url)
-    return response.text
+url_word_counts = {}
+frequencies = {}
+files = {}
 
-def count_word_frequencies(url, word):
-    text = get_text(url)
-    words = text.split()
-    count = 0
-    for w in words:
-        if w == word:
-            count += 1
-    return count
+async def main(url, words_file):
 
-def main():
-    words_file = "words.txt"
-    url = "https://eng.mipt.ru/why-mipt/"
+    # хеширование запроса
+    if url not in url_word_counts:
+        url_word_counts[url] = {}
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                data = await response.text()
 
-    words_to_count = []
-    with open(words_file, 'r') as file:
-        for line in file:
-            word = line.strip()
-            if word:
-                words_to_count.append(word)
+        # создание словаря содержания запроса {слово: количество в responde}
+        for word in data.split():
+            if word not in url_word_counts[url]:
+                url_word_counts[url][word] = 1
+            else:
+                url_word_counts[url][word] += 1
 
-    frequencies = {}
-    for word in words_to_count:
-        frequencies[word] = count_word_frequencies(url, word)
+    # хеширование открытия файла в сет:
+    if words_file not in files:
+        files[words_file] = set()
+        with open(words_file, 'r') as file:
+            for line in file:
+                word = line.strip()
+                files[words_file].add(word)
+
+    for word in files[words_file]:
+        try:
+            frequencies[word] = url_word_counts[url][word]
+        except KeyError as e: # если нет слова из words в ответе url
+            frequencies[word] = 0
     
     print(frequencies)
+    return frequencies
+
+words_file = "words.txt"
+url = "https://eng.mipt.ru/why-mipt/"
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main(url, words_file))
